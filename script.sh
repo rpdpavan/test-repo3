28283bf72306e325b4c10fbe402cc1a17cab3c05
rpdpavan <rayaprolupavand@gmail.com>
2025-09-08 12:42:10 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "hi, how are you, what are you doing?, I'm doing fine, hey what are you doing where are you?"
+echo "hi, how are you, what are you doing?, I'm doing fine, hey what are you doing where are you? fine"
 echo "well hi, how are you, what are you doing?, I'm doing fine, what are you doing where are you hey ? I'm fine"
